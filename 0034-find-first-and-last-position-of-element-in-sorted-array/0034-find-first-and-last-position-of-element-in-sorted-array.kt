6d8b8bb3e492b0f2abee446e94f381d05473c5ce
mog <juhe1998@naver.com>
2025-11-09 23:10:58 +0900

    Time: 0 ms (100%), Space: 47.7 MB (48.4%) - LeetHub

---
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.kt
@@ -0,0 +1,32 @@
+class Solution {
+    fun searchRange(nums: IntArray, target: Int): IntArray {
+        var left = 0
+        var right = nums.size - 1
+        while (left <= right) {
+            val mid = (left + right) / 2
+            val temp = nums[mid]
+            
+            if (temp == target) {
+                left = mid 
+                right = mid
+
+                while (left - 1 >= 0 && nums[left -1] == target) {
+                    left--
+                }
+
+                while (right + 1 < nums.size && nums[right + 1] == target) {
+                    right++
+                }
+
+                return intArrayOf(left, right)
+
+            } else if (temp > target) {
+                right = mid -1
+            } else {
+                left = mid + 1
+            }
+        }
+
+        return intArrayOf(-1, -1)
+    }
+}
