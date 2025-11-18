d30b0722812c2efb635f97aaa15f94d755786e1c
mog <juhe1998@naver.com>
2025-11-19 01:23:06 +0900

    Time: 4 ms (22.01%), Space: 61.8 MB (87.7%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.kt
+++ 0053-maximum-subarray/0053-maximum-subarray.kt
@@ -1,25 +1,12 @@
 class Solution {
     fun maxSubArray(nums: IntArray): Int {
+        var result = nums[0]
+        var minPrefix = 0
         var sum = 0
-        val sumList = mutableListOf<Int>()
         for (num in nums) {
             sum += num
-            sumList.add(sum)
-        }
-
-        var left = 0
-        var result = sumList[0]
-
-        var min = 0
-
-        for (s in sumList) {
-            if (s - min > result) {
-                result = s - min
-            }
-            if (sumList[left] < min) {
-                min = sumList[left]
-            }
-            left ++ 
+            result = maxOf(result, sum - minPrefix)
+            minPrefix = minOf(minPrefix, sum)
         }
 
         return result
