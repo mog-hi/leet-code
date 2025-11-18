861731a7078b19ed8a98fcb98ea8160613c0c3f5
mog <juhe1998@naver.com>
2025-11-19 00:04:14 +0900

    Time: 3 ms (85.09%), Space: 64.6 MB (86.75%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.kt
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.kt
@@ -1,14 +1,17 @@
 class Solution {
     fun maxProfit(prices: IntArray): Int {
-        var min = prices[0]
-        var result = 0
-        for (price in prices) {
-            if (min > price) {
-                min = price
-            } else if (price - min > result ) {
-                result = price - min
+        var maxProfit = 0
+        var left = 0
+        var minLeftValue = prices[0]
+        for (right in 1 until prices.size) {
+            if (prices[left] < minLeftValue) {
+                minLeftValue = prices[left]
+            } 
+            if (prices[right] - minLeftValue > maxProfit) {
+                maxProfit = prices[right] - minLeftValue
             }
+            left++
         }
-        return result
+        return maxProfit
     }
 }
