9d0474b7f31ae290fae680d6f3c52299a7088b2a
mog <juhe1998@naver.com>
2025-11-20 01:04:23 +0900

    Time: 18 ms (55.69%), Space: 46.7 MB (23.14%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.kt
@@ -0,0 +1,30 @@
+class Solution {
+    fun isValid(s: String): Boolean {
+        val stack = ArrayDeque<Char>()
+
+        for (temp in s) {
+            if (isLeft(temp)) {
+                stack.addLast(temp)
+            } else {
+                if (stack.isEmpty()) {
+                    return false
+                }
+
+                val left = stack.removeLast()
+                if (!isPair(left, temp)) {
+                    return false
+                }
+            }
+        }
+
+        return stack.isEmpty()
+    }
+}
+
+fun isLeft(s: Char): Boolean = s == '(' || s == '{' || s== '['
+
+fun isPair(left: Char, right: Char): Boolean = if (
+    (left == '[' && right == ']') ||
+    (left == '{' && right == '}') ||
+    (left == '(' && right == ')') 
+) true else false
