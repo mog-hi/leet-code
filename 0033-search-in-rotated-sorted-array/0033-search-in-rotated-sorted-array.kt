239e634a3cf8d184fde8d2a7e3c31c5316e3fe02
mog <juhe1998@naver.com>
2025-11-25 00:20:43 +0900

    Time: 0 ms (100%), Space: 42.3 MB (68.75%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.kt
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.kt
@@ -6,28 +6,27 @@ class Solution {
         while (left <= right) {
             val mid = (left + right) / 2
 
-
-            println("$left $mid $right")
-
-            println("${nums[left]} ${nums[mid]} ${nums[right]}")
-
             if (target == nums[mid]) {
                 return mid
             } 
-            // 구간의 오른 쪽에 있다. left = mid + 1
-            else if ((target > nums[mid] && target <= nums[right]) || (target < nums[mid] && target < nums[left] &&nums[mid] > nums[right] ) || (target > nums[mid] && nums[mid] > nums[right])) {
-                println("right")
-                left = mid + 1
+
+            // 왼쪽이 정렬되어 있다
+            if (nums[left] <= nums[mid]) {
+                if (target >= nums[left] && target < nums[mid]) {
+                    right = mid -1
+                } else {
+                    left = mid + 1
+                }
             } else {
-                println("left")
-                right = mid - 1
+                // 오른쪽이 정렬되어 있다 
+                if (target > nums[mid] && target <= nums[right]) {
+                    left = mid + 1
+                } else {
+                    right = mid -1
+                }
             }
-
-
-            // 구간의 왼쪽에 있다.
-            // if ((target > nums[mid] && target > nums[right]) || )
-
         }
+        
         return -1
     }
 }
