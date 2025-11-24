b0c4ab8aaea3d50af604dc32afeb570cfc33c09c
mog <juhe1998@naver.com>
2025-11-25 00:55:12 +0900

    Time: 3 ms (68.33%), Space: 51.7 MB (60.17%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.kt
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.kt
@@ -2,35 +2,47 @@ class Solution {
     fun findMedianSortedArrays(nums1: IntArray, nums2: IntArray): Double {
         val n = nums1.size
         val m = nums2.size
-        val mergedArray = mutableListOf<Int>()
+        val mergedArray = IntArray(n + m)
 
         var i = 0
         var j = 0
+        var insertPoint = 0
 
         while (i < n && j < m) {
+            if (insertPoint > (n+m)/2) {
+                return mergedArray.findMedian()
+            }
             if (nums1[i] < nums2[j]) {
-                mergedArray.add(nums1[i])
+                mergedArray[insertPoint] = nums1[i]
                 i++
             } else {
-                mergedArray.add(nums2[j])
+                mergedArray[insertPoint] = nums2[j]
                 j++
             }
+            insertPoint++
         }
 
         while (i < n) {
-            mergedArray.add(nums1[i])
+            mergedArray[insertPoint] = nums1[i]
             i++ 
+            insertPoint++
         }
 
         while (j < m) {
-            mergedArray.add(nums2[j])
+            mergedArray[insertPoint] = nums2[j]
             j++
+            insertPoint++
         }
 
-        return if ((n + m)%2 == 0) {
-            (mergedArray[(n+m)/2 -1] + mergedArray[(n+m)/2].toDouble()) / 2
+        return mergedArray.findMedian()
+    }
+
+    private fun IntArray.findMedian(): Double {
+        val n = this.size
+        return if (n % 2 == 0) {
+            (this[n / 2 - 1] + this[n/2].toDouble()) / 2
         } else {
-            mergedArray[(n+m-1)/2].toDouble()
+            this[(n - 1) / 2].toDouble()
         }
     }
 }
