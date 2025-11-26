ecf5d11936397c545365be543b0e0636ba5956ca
mog <juhe1998@naver.com>
2025-11-27 00:40:27 +0900

    Time: 172 ms (49.48%), Space: 48.9 MB (96.91%) - LeetHub

---
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.kt
@@ -0,0 +1,47 @@
+/**
+ * Example:
+ * var li = ListNode(5)
+ * var v = li.`val`
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int) {
+ *     var next: ListNode? = null
+ * }
+ */
+
+class Solution {
+    fun getIntersectionNode(headA:ListNode?, headB:ListNode?):ListNode? {
+        // idea1: 결국 끝점에서 만난다. 끝점까지 온 다음 역으로 가서 갈라지기 직전 노드 리턴
+        // 문제: 역으로 갈 방법이 없음
+
+        // idea2: (힌트 받음) lastA에선 headB로, lastB에서 headA로 가게 하면 같은 거리 이동 후 intersect에서 서로 만나게됨
+
+        var pointerA = headA
+        var pointerB = headB
+
+        var isPointerAVisitedHeadB = false
+        var isPointerBVisitedHeadA = false
+
+        while(pointerA != pointerB) {
+            if (pointerA?.next == null) {
+                if (isPointerAVisitedHeadB) return null
+
+                pointerA = headB
+                isPointerAVisitedHeadB = true
+            } else {
+                pointerA = pointerA?.next
+            }
+
+
+            if (pointerB?.next == null) {
+                if (isPointerBVisitedHeadA) return null
+
+                pointerB = headA
+                isPointerBVisitedHeadA = true
+            } else {
+                pointerB = pointerB?.next
+            }
+        }
+
+        return pointerA
+    }
+}
