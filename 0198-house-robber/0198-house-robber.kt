9e4c3312cc94adb8437413c55aa6fe1494fcc2c5
mog <juhe1998@naver.com>
2025-11-28 22:00:54 +0900

    Time: 0 ms (100%), Space: 40.4 MB (86.86%) - LeetHub

--- 0198-house-robber/0198-house-robber.kt
+++ 0198-house-robber/0198-house-robber.kt
@@ -14,9 +14,7 @@ class Solution {
         robs[2] = nums[0] + nums[2]
 
         for (i in 3 .. n-1) {
-            println(i)
             robs[i] = maxOf(robs[i-2], robs[i-3]) + nums[i]
-            println(robs[i])
         }
 
         return maxOf(robs[n-1], robs[n-2])
