0656f48c0a2b86396c10fb7697bbac397f510c9b
mog <juhe1998@naver.com>
2025-11-30 02:14:38 +0900

    Time: 9 ms (65.07%), Space: 47.1 MB (80.8%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.kt
+++ 0039-combination-sum/0039-combination-sum.kt
@@ -1,22 +1,22 @@
 class Solution {
     fun combinationSum(candidates: IntArray, target: Int): List<List<Int>> {
-        val result: MutableSet<List<Int>> = mutableSetOf()
+        val result: MutableList<List<Int>> = mutableListOf()
 
-        fun backtrack(sum: Int, combinations: MutableList<Int>) {
+        fun backtrack(sum: Int, start: Int, combinations: MutableList<Int>) {
             if (sum == target) {
-                result.add(combinations.sorted())
+                result.add(combinations.toList())
             }
 
-            for (num in candidates) {
-                if (sum + num <= target) {
-                    combinations.add(num)
-                    backtrack(sum+num, combinations)
+            for (i in start until candidates.size) {
+                if (sum + candidates[i] <= target) {
+                    combinations.add(candidates[i])
+                    backtrack(sum + candidates[i], i, combinations)
                     combinations.removeLast()
                 }
             }
         }
         
-        backtrack(0, mutableListOf())
+        backtrack(0, 0, mutableListOf())
         return result.toList()
     }
 }
