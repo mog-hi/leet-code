ab62796fc709097e204f9d56c556984531870b1f
mog <juhe1998@naver.com>
2025-11-30 01:52:25 +0900

    Time: 7 ms (78.75%), Space: 43.6 MB (70.63%) - LeetHub

--- 0078-subsets/0078-subsets.kt
+++ 0078-subsets/0078-subsets.kt
@@ -1,26 +1,18 @@
 class Solution {
     fun subsets(nums: IntArray): List<List<Int>> {
-        val used = BooleanArray(nums.size)
         val result: MutableList<List<Int>> = mutableListOf()
 
-        fun backtrack(subsets: MutableList<Int>) {
-            println(subsets)
+        fun backtrack(prev: Int, subsets: MutableList<Int>) {
             result.add(subsets.toList())
             
-            for (i in nums.indices) {
-                if (used[i]) continue
-                if (subsets.size == 0 || (subsets.size > 0 && i > subsets.last())) {
-                    subsets.add(i)
-                    used[i] = true
-                    // println(subsets)
-                    backtrack(subsets)
-                    used[i] = false
-                    subsets.removeLast()
-                }
+            for (i in prev until nums.size) {
+                subsets.add(nums[i])
+                backtrack(i + 1, subsets)
+                subsets.removeLast()
             }
         }
 
-        backtrack(mutableListOf())
-        return result.map { subset -> subset.map { nums[it] } }
+        backtrack(0, mutableListOf())
+        return result
     }
 }
