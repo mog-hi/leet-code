8142709e3dc54d72ecfb454c579b767a6f766d5b
mog <juhe1998@naver.com>
2025-11-29 15:59:56 +0900

    Time: 18 ms (69.06%), Space: 44.6 MB (93.25%) - LeetHub

--- 0322-coin-change/0322-coin-change.kt
+++ 0322-coin-change/0322-coin-change.kt
@@ -1,15 +1,11 @@
 class Solution {
     fun coinChange(coins: IntArray, amount: Int): Int {
         val ways = IntArray(amount + 1) { Int.MAX_VALUE }
-
         ways[0] = 0
-        for (coin in coins) { 
-            if (coin <= amount) ways[coin] = 1
-        }
 
         for (i in 1 .. amount) {
             for (coin in coins) {
-                if (i - coin > 0 && ways[i - coin] < Int.MAX_VALUE) {
+                if (i - coin >= 0 && ways[i - coin] < Int.MAX_VALUE) {
                     ways[i] = minOf(ways[i - coin] + 1, ways[i])
                 }
             }
