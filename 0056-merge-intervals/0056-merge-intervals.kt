7a280fb1d3ef36692f4aa871e0407cee2d66f98b
mog <juhe1998@naver.com>
2025-12-03 00:04:57 +0900

    Time: 22 ms (69.64%), Space: 64 MB (11.67%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.kt
+++ 0056-merge-intervals/0056-merge-intervals.kt
@@ -1,13 +1,13 @@
 class Solution {
     fun merge(intervals: Array<IntArray>): Array<IntArray> {
-        val sortedIntervals = intervals.toList().sortedBy { it.first() }
+        intervals.sortBy { it[0] } 
 
-        var left = sortedIntervals[0][0]
-        var right = sortedIntervals[0][1]
+        var left = intervals[0][0]
+        var right = intervals[0][1]
         val answer: MutableList<IntArray> = mutableListOf()
 
         for (i in 1 until intervals.size) {
-            val (start, end) = sortedIntervals[i]
+            val (start, end) = intervals[i]
 
             if (start <= right) {
                 right = maxOf(right, end)
